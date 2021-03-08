022dd08711f1a2d43dbbd2f22da0b9f4fa873de4
Wrik Das <wrikdas22@gmail.com>
2021-03-08 22:59:31 +0530

    Implemented tab switching functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-console.log('Looking good!');
+import homeModule from './home';
+import contactModule from './contact'
+import menuModule from './menu';
+
+function controller() {
+
+    const home = document.querySelector('#home');
+    home.addEventListener('click', (event) => {
+        removeExistingElements();
+        homeModule();
+    });
+
+    const menu = document.querySelector('#menu');
+    menu.addEventListener('click', (event) => {
+        removeExistingElements();
+        menuModule();
+    });
+
+    const contact = document.querySelector('#contact');
+    contact.addEventListener('click', (event) => {
+        removeExistingElements();
+        contactModule();
+    });
+
+    function removeExistingElements() {
+
+        const buttons = [ ...document.querySelectorAll('.buttons') ];
+        buttons.forEach(button => button.classList.remove('current-tab'));
+
+        const content = document.querySelector('#content');
+        const cardContainer = document.querySelector('.card-container');
+        const main = document.querySelector('.main');
+
+        if (cardContainer !== null) content.removeChild(cardContainer);
+        if (main !== null) content.removeChild(main);
+    }
+
+    home.click();
+
+}
+
+controller();
